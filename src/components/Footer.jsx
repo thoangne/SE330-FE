dba829ca1e96a97f018b306979045bc5f369e546
Zoraneko <tuandt1409@gmail.com>
2025-04-28 15:14:07 +0700

    Folder Structure Example

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ function Footer() {
     <footer className="bg-light py-3">
       <Container>
         <p className="text-center m-0">© 2025 SE330-FE - Fahasa Clone</p>
+        
       </Container>
     </footer>
   );
